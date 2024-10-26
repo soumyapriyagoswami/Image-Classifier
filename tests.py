2723a331be8660a9dd5f8af79fe1c099bf45cdbe
jschulz8138 <115745519+jschulz8138@users.noreply.github.com>
2024-10-26 17:43:54 -0400

    Update tests.py
    Added some basic unit tests

--- tests.py
+++ tests.py
@@ -2,11 +2,20 @@ import pytest
 import result.py
 import "model creation.py"
 import app.py
+import cv
 
-def test_calculate_area_square():
-    assert calculate_area_square(2) == 4
-    assert calculate_area_square(2.5) == 6.25
+def preprocess_image_height():
+    result = preprocess_image(img_path)
+    assert result.shape[0] == 32
+    
+def preprocess_image_width():
+    result = preprocess_image(img_path)
+    assert result.shape[1] == 32
 
-def test_calculate_area_square_negative():
-    with pytest.raises(TypeError):  
-        calculate_area_square(-2)
+def preprocess_image_return_type():
+    result = preprocess_image(img_path)
+    assert type(result) is uint8
+    
+def result_return_type():
+    result = result(img_path)
+    assert type(result) is uint8
