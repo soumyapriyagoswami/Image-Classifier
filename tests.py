abaff13669502390704efc581868397c769b543e
stank3391 <115744916+stank3391@users.noreply.github.com>
2024-10-26 18:07:50 -0400

    Update tests.py
    Added a dynamic test for multiple images

--- tests.py
+++ tests.py
@@ -19,3 +19,31 @@ def preprocess_image_return_type():
 def result_return_type():
     result = result(img_path)
     assert type(result) is uint8
+
+def dynamic_result_test():
+    class_names = ["plane", "car", "bird", "cat", "deer", "dog", "frog", "horse", "ship", "truck"]
+    image_path_dict = {
+        "test_img/plane0" : class_names[0],
+        "test_img/plane1" : class_names[0],
+        "test_img/car0" : class_names[1],
+        "test_img/car1" : class_names[1],
+        "test_img/bird0" : class_names[2],
+        "test_img/bird1" : class_names[2],
+        "test_img/cat0" : class_names[3],
+        "test_img/cat1" : class_names[3],
+        "test_img/deer0" : class_names[4],
+        "test_img/deer1" : class_names[4],
+        "test_img/dog0" : class_names[5],
+        "test_img/dog1" : class_names[5],
+        "test_img/frog0" : class_names[6],
+        "test_img/frog1" : class_names[6],
+        "test_img/horse0" : class_names[7],
+        "test_img/horse1" : class_names[7],
+        "test_img/ship0" : class_names[8],
+        "test_img/ship1" : class_names[8],
+        "test_img/truck0" : class_names[9],
+        "test_img/truck1" : class_names[9]
+    }
+    for (key, value in image_path_dict.items()):
+        result = result(key)
+        assert result == value
