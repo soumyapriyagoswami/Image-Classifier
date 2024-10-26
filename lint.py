60ed97f29f41b5bce777d07b6719c41ae2ad1e83
jschulz8138 <115745519+jschulz8138@users.noreply.github.com>
2024-10-26 16:16:16 -0400

    Create lint.py
    Created lint.py, a file that lints each one of our .py files

---
+++ lint.py
@@ -0,0 +1,24 @@
+#lint.py 
+import sys 
+from pylint import lint  
+THRESHOLD = 9  
+
+#Pylint for Application 
+run = lint.Run(["app.py"], do_exit=False) 
+appScore = run.linter.stats["global_note"]  
+if appScore < THRESHOLD: 
+    print("Linter failed: appScore < threshold value") 
+
+#Pylint for Model Creation
+run = lint.Run(["model creation.py"], do_exit=False) 
+modelScore = run.linter.stats["global_note"]  
+if modelScore < THRESHOLD: 
+    print("Linter failed: modelScore < threshold value") 
+
+#Pylint for Results
+run = lint.Run(["result.py"], do_exit=False) 
+resultScore = run.linter.stats["global_note"]  
+if resultScore < THRESHOLD: 
+    print("Linter failed: resultScore < threshold value") 
+
+sys.exit(0) 
