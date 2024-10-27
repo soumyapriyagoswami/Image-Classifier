0fa2db2d433e49681d444733edd4a919aa8e6f66
stank3391 <115744916+stank3391@users.noreply.github.com>
2024-10-27 19:38:23 -0400

    Update result.py
    Removed trailing whitespace

--- result.py
+++ result.py
@@ -10,7 +10,7 @@ def result(filename: str ="static/uploads/uploaded_image.png") -> str:
     This function handles the grabbing of the preprocessed data and spits out a predictions.
     """
     # Load CIFAR-10 dataset
-    (training_images,_training_labels),(testing_images,_testing_labels,)=datasets.cifar10.load_data() 
+    (training_images,_training_labels),(testing_images,_testing_labels,)=datasets.cifar10.load_data()
     training_images, testing_images = training_images / 255, testing_images / 255
     # Class names for CIFAR-10
     class_names = ["plane","car","bird","cat","deer","dog","frog","horse","ship","truck"]
