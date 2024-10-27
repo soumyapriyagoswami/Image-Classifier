179d666e75e5b815731aca2bcf8b5eda35926f72
stank3391 <115744916+stank3391@users.noreply.github.com>
2024-10-27 19:15:42 -0400

    Update tests.py
    Fixed intents

--- tests.py
+++ tests.py
@@ -5,23 +5,23 @@ from result import result
 from app import preprocess_image
 
 def preprocess_image_height():
-"""This test case handles loading an image, checking if the new height is equal"""
+    """This test case handles loading an image, checking if the new height is equal"""
     assert preprocess_image("test_img/plane0.png").shape[0] == 32
 
 def preprocess_image_width():
-"""This test case handles loading an image and checking if the new width is equal to the expected value."""
+    """This test case handles loading an image and checking if the new width is equal to the expected value."""
     assert preprocess_image("test_img/plane0.png").shape[1] == 32
 
 def preprocess_image_return_type():
-"""This test case handles loading an image and checking the return type."""
+    """This test case handles loading an image and checking the return type."""
     assert isinstance(preprocess_image("test_img/plane0.png"), np.uint8)
 
 def result_return_type():
-"""This test case handles entire functionality of result and checks the return type"""
+    """This test case handles entire functionality of result and checks the return type"""
     assert isinstance(result("test_img/plane0"), np.uint8)
 
 def dynamic_result_test():
-"""This test case is a dynamic test, testing two of each image to ensure the model is trained and predicting correctly"""
+    """This test case is a dynamic test, testing two of each image to ensure the model is trained and predicting correctly"""
     class_names = ["plane", "car", "bird", "cat", "deer", "dog", "frog", "horse", "ship", "truck"]
     image_path_dict = {
         "test_img/plane0.png" : class_names[0],
