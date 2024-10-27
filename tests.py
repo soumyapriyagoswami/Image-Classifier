dc13afbcf1473f7c04c29e4771eecad747fd7d17
stank3391 <115744916+stank3391@users.noreply.github.com>
2024-10-27 19:12:40 -0400

    Update tests.py
    Fixing pylint errors.

--- tests.py
+++ tests.py
@@ -1,42 +1,27 @@
-"""
-This module handles the testing of the functionality of the system.
-"""
+"""This module handles the testing of the functionality of the system."""
+import numpy as np
 import pytest
 from result import result
 from app import preprocess_image
 
-"""
-This test case handles loading an image, checking if the new height is equal
-"""
 def preprocess_image_height():
-    result = preprocess_image("test_img/plane0.png")
-    assert result.shape[0] == 32
+"""This test case handles loading an image, checking if the new height is equal"""
+    assert preprocess_image("test_img/plane0.png").shape[0] == 32
 
-"""
-This test case handles loading an image and checking if the new width is equal to the expected value.
-"""
 def preprocess_image_width():
-    result = preprocess_image("test_img/plane0.png")
-    assert result.shape[1] == 32
+"""This test case handles loading an image and checking if the new width is equal to the expected value."""
+    assert preprocess_image("test_img/plane0.png").shape[1] == 32
 
-"""
-This test case handles loading an image and checking the return type.
-"""
 def preprocess_image_return_type():
-    result = preprocess_image("test_img/plane0.png")
-    assert type(result) is uint8
-    
-"""
-This test case handles entire functionality of result and checks the return type
-"""
-def result_return_type():
-    result = result("test_img/plane0")
-    assert type(result) is uint8
+"""This test case handles loading an image and checking the return type."""
+    assert isinstance(preprocess_image("test_img/plane0.png"), np.uint8)
 
-"""
-This test case is a dynamic test, testing two of each image to ensure the model is trained and predicting correctly
-"""
+def result_return_type():
+"""This test case handles entire functionality of result and checks the return type"""
+    assert isinstance(result("test_img/plane0"), np.uint8)
+
 def dynamic_result_test():
+"""This test case is a dynamic test, testing two of each image to ensure the model is trained and predicting correctly"""
     class_names = ["plane", "car", "bird", "cat", "deer", "dog", "frog", "horse", "ship", "truck"]
     image_path_dict = {
         "test_img/plane0.png" : class_names[0],
@@ -61,5 +46,4 @@ def dynamic_result_test():
         "test_img/truck1.png" : class_names[9]
     }
     for key, value in image_path_dict.items():
-        result = result(key)
-        assert result == value
+        assert result(key) == value
