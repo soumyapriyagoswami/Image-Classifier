d6a0bb6002b7182c19c7b7ad2c9983db9d73d08d
stank3391 <115744916+stank3391@users.noreply.github.com>
2024-10-27 19:34:49 -0400

    Update tests.py
    Fixed pylint errors

--- tests.py
+++ tests.py
@@ -1,6 +1,5 @@
 """This module handles the testing of the functionality of the system."""
 import numpy as np
-import pytest
 from result import result
 from app import preprocess_image
 
@@ -9,7 +8,7 @@ def preprocess_image_height():
     assert preprocess_image("test_img/plane0.png").shape[0] == 32
 
 def preprocess_image_width():
-    """This test case handles loading an image and checking if the new width is equal to the expected value."""
+    """loads an image.Checks if the new width is equal to the expected value."""
     assert preprocess_image("test_img/plane0.png").shape[1] == 32
 
 def preprocess_image_return_type():
@@ -21,7 +20,8 @@ def result_return_type():
     assert isinstance(result("test_img/plane0"), np.uint8)
 
 def dynamic_result_test():
-    """This test case is a dynamic test, testing two of each image to ensure the model is trained and predicting correctly"""
+    """This test case is a dynamic test
+    testing two of each image to ensure the model is trained and predicting correctly"""
     class_names = ["plane", "car", "bird", "cat", "deer", "dog", "frog", "horse", "ship", "truck"]
     image_path_dict = {
         "test_img/plane0.png" : class_names[0],
