a331b5454049c8457c5a8b1a81a8cffe7522cd2b
stank3391 <115744916+stank3391@users.noreply.github.com>
2024-10-27 20:08:34 -0400

    Update classifier_test.py
    Fixed file patss

--- classifier_test.py
+++ classifier_test.py
@@ -9,45 +9,45 @@ warnings.filterwarnings("ignore", category=DeprecationWarning)
 
 def test_preprocess_image_height():
     """This test case handles loading an image, checking if the new height is equal"""
-    assert preprocess_image("test_img/plane0.png").shape[0] == 32
+    assert preprocess_image("static/test_img/plane0.png").shape[0] == 32
 
 def test_preprocess_image_width():
     """loads an image.Checks if the new width is equal to the expected value."""
-    assert preprocess_image("test_img/plane0.png").shape[1] == 32
+    assert preprocess_image("static/test_img/plane0.png").shape[1] == 32
 
 def test_preprocess_image_return_type():
     """This test case handles loading an image and checking the return type."""
-    assert isinstance(preprocess_image("test_img/plane0.png"), np.uint8)
+    assert isinstance(preprocess_image("static/test_img/plane0.png"), np.uint8)
 
 def text_result_return_type():
     """This test case handles entire functionality of result and checks the return type"""
-    assert isinstance(result("test_img/plane0"), np.uint8)
+    assert isinstance(result("static/test_img/plane0"), np.uint8)
 
 def test_dynamic_result_test():
     """This test case is a dynamic test
     testing two of each image to ensure the model is trained and predicting correctly"""
     class_names = ["plane", "car", "bird", "cat", "deer", "dog", "frog", "horse", "ship", "truck"]
     image_path_dict = {
-        "test_img/plane0.png" : class_names[0],
-        "test_img/plane1.png" : class_names[0],
-        "test_img/car0.png" : class_names[1],
-        "test_img/car1.png" : class_names[1],
-        "test_img/bird0.png" : class_names[2],
-        "test_img/bird1.png" : class_names[2],
-        "test_img/cat0.png" : class_names[3],
-        "test_img/cat1.png" : class_names[3],
-        "test_img/deer0.png" : class_names[4],
-        "test_img/deer1.png" : class_names[4],
-        "test_img/dog0.png" : class_names[5],
-        "test_img/dog1.png" : class_names[5],
-        "test_img/frog0.png" : class_names[6],
-        "test_img/frog1.png" : class_names[6],
-        "test_img/horse0.png" : class_names[7],
-        "test_img/horse1.png" : class_names[7],
-        "test_img/ship0.png" : class_names[8],
-        "test_img/ship1.png" : class_names[8],
-        "test_img/truck0.png" : class_names[9],
-        "test_img/truck1.png" : class_names[9]
+        "static/test_img/plane0.png" : class_names[0],
+        "static/test_img/plane1.png" : class_names[0],
+        "static/test_img/car0.png" : class_names[1],
+        "static/test_img/car1.png" : class_names[1],
+        "static/test_img/bird0.png" : class_names[2],
+        "static/test_img/bird1.png" : class_names[2],
+        "static/test_img/cat0.png" : class_names[3],
+        "static/test_img/cat1.png" : class_names[3],
+        "static/test_img/deer0.png" : class_names[4],
+        "static/test_img/deer1.png" : class_names[4],
+        "static/test_img/dog0.png" : class_names[5],
+        "static/test_img/dog1.png" : class_names[5],
+        "static/test_img/frog0.png" : class_names[6],
+        "static/test_img/frog1.png" : class_names[6],
+        "static/test_img/horse0.png" : class_names[7],
+        "static/test_img/horse1.png" : class_names[7],
+        "static/test_img/ship0.png" : class_names[8],
+        "static/test_img/ship1.png" : class_names[8],
+        "static/test_img/truck0.png" : class_names[9],
+        "static/test_img/truck1.png" : class_names[9]
     }
     for key, value in image_path_dict.items():
         assert result(key) == value
