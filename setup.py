ba6f90f20e7252f0caebbf97c0e13de76fb0bf0f
jschulz8138 <115745519+jschulz8138@users.noreply.github.com>
2024-11-20 17:37:17 -0500

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,33 @@
+import os
+
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="groupX",
+    version="1.0.0",
+    author="groupX",
+    description="CSCN73010: image classifier ",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/jschulz8138/Image-Classifier",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+    ],
+    install_requires=[
+        "flask",
+        "numpy",
+        "opencv-python",
+        "matplotlib",
+        "tensorflow",
+    ],
+    extras_require={
+        "test": [
+            "pytest",
+        ],
+    },
+    python_requires=">=3.11",
+)
